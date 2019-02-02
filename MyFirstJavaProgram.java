847c566c037f158745a1fef20feb3c64a680eaf0
James Sanii <james@ip-172-31-8-86.ca-central-1.compute.internal>
2019-02-02 22:33:48 +0000

    Hello World done in java

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,18 @@
+/****************************************************************************
+ *
+ * Created by: James Lee
+ * Created on: Feb 2018
+ * This program displays Hello, World!
+ *
+ ****************************************************************************/
+
+public class MyFirstJavaProgram {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World"); 
+    }
+
+}
+
+
